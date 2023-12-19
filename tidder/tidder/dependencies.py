48a256a191ebbfcc46293a53eb2d2db634d790bd
Leonardo Schettini <leoschettini2@gmail.com>
2023-12-19 23:38:22 +0100

    feat: matplotlib as optional dependency

--- tidder/tidder/dependencies.py
+++ tidder/tidder/dependencies.py
@@ -14,17 +14,22 @@ from typing import TYPE_CHECKING
 from polars.dependencies import _lazy_import
 
 _WEBVTT_AVAILABLE = True
+_PYPLOT_AVAILABLE = True
 
 # Handle static type checking
 # https://docs.python.org/3/library/typing.html#typing.TYPE_CHECKING
 if TYPE_CHECKING:
     import webvtt
+    from matplotlib import pyplot
 else:
     webvtt, _WEBVTT_AVAILABLE = _lazy_import("webvtt")
+    pyplot, _PYPLOT_AVAILABLE = _lazy_import("matplotlib.pyplot")
 
 __all__ = [
     # Lazy-loaded modules
     "webvtt",
+    "pyplot",
     # Flags
     "_WEBVTT_AVAILABLE",
+    "_PYPLOT_AVAILABLE",
 ]
